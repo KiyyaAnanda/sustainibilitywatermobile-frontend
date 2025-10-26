fa67243fd87c38e88323177622cc16750ac69c48
ffffffff3rnan <lulabyblueeblackberry@gmail.com>
2025-10-26 21:03:13 +0700

    backend

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -11,12 +11,12 @@ import axios from "axios";
 // WIFI Zidan
 //export const API_URL = "http://10.127.212.240:5255/";
 
-export const API_URL = "https://sia-pt.polytechnic.astra.ac.id/stn-air/";
+//export const API_URL = "https://sia-pt.polytechnic.astra.ac.id/stn-air/";
 
 // Wifi Daffa
 // export const API_URL = "http://192.168.1.8:5255/";
 
-//export const API_URL = "http://192.168.1.199:5255/";
+export const API_URL = "http://192.168.1.16:5255/";
 
 const apiClient = axios.create({
   baseURL: API_URL,
